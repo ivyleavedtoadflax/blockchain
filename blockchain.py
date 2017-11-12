53759629b54bb4b4d556a6a81ff5a510843632de
Matthew Upson <matthew.a.upson@gmail.com>
2017-11-12 15:20:30 +0000

    One script version

--- blockchain.py
+++ blockchain.py
@@ -9,16 +9,18 @@ import json
 from time import time
 from uuid import uuid4
 
+from flask import Flask, jsonify, request
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
-        self.current_transaction = []
+        self.current_transactions = []
 
         # Create the genesis block
 
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
 
@@ -106,5 +108,70 @@ class Blockchain(object):
         guess_hash = hashlib.sha384(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+# Instatntiate node
 
+app = Flask(__name__)
 
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+
+blockchain = Blockchain()
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    requried = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    
+    # Run the proof of work algorithm to get the next proof
+    
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Receive a reward for finding the proof
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Fogre the new Block by adding it to the chain
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
