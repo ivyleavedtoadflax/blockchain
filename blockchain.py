18e1223b0b0d2c0533017f8593b36949f6fe3f08
Matthew Upson <matthew.a.upson@gmail.com>
2017-11-12 12:58:06 +0000

    Add new_block

--- blockchain.py
+++ blockchain.py
@@ -3,14 +3,41 @@ Simple DIY blockchain from:
 https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 """
 
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
 
-    def new_block(self):
+        # Create the genesis block
 
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self):
+        """
+        Create a new Block in the Blockchain
+
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of the previous block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp' : time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
